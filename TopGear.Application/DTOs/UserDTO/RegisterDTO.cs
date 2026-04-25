0b8e65f43c739b8fa147e98b298b9c3b4197edd3
Aayush Karki <karkiaayush01@gmail.com>
2026-04-25 21:29:51 +0545

    Feat: Initial authentication implementation - JWT implementation - JwtTokenService added for generating user token - First DTO - Auth Controller with Services

---
+++ TopGear.Application/DTOs/UserDTO/RegisterDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace TopGear.Application.DTOs.UserDTO;
+
+public class RegisterDTO
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    
+    // Phone number is optional
+    public string? PhoneNumber { get; set; } = null;
+}
---
+++ TopGear.Infrastructure/Auth/JwtTokenService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using TopGear.Application.Interfaces;
+using TopGear.Domain.Entities;
+using TopGear.Infrastructure.Auth;
+
+namespace TopGear.Application.Services;
+
+public class JwtTokenService(IOptions<JwtOptions> jwtOptions, UserManager<User> userManager) : IJwtTokenService
+{
+    private readonly JwtOptions _jwtOptions = jwtOptions.Value;
+
+    public async Task<String> GenerateUserToken(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new(JwtRegisteredClaimNames.Email, user.Email!)
+        };
+
+        // Add roles in claim
+        var roles = await userManager.GetRolesAsync(user);
+        string role = roles.FirstOrDefault() ?? "Customer";
+        claims.Add(new Claim(ClaimTypes.Role, role));
+
+        return GenerateToken(claims);
+    }
+
+    public string GenerateToken(IEnumerable<Claim> claims)
+    {
+        var credentials = new SigningCredentials(_jwtOptions.SymmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _jwtOptions.Issuer,
+            audience: _jwtOptions.Audience,
+            claims: claims,
+            expires: _jwtOptions.ExpiryDate,
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
