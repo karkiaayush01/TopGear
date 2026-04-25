3d88074db39649294cd4462f469ae22b8861852b
Aayush Karki <karkiaayush01@gmail.com>
2026-04-25 23:35:27 +0545

    Feat: Register API - Register API Complete - Fix(Register): Username and Email are equal now - Added Swagger UI for quick testings

--- TopGear/Controllers/AuthController.cs
+++ TopGear/Controllers/AuthController.cs
@@ -1,19 +1,25 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TopGear.Application.DTOs.UserDTO;
+using TopGear.Application.Interfaces;
 
 namespace TopGear.Controllers;
 
 [ApiController]
 [Route("api/auth")]
-public class AuthController : ControllerBase
+public class AuthController(IAuthService authService) : ControllerBase
 {
     /// <summary>
-    /// Self Register Endpoint for a customer
+    /// Self Signup Endpoint for a customer
     /// </summary>
-    [HttpPost("register")]
-    public async Task SelfRegisterCustomer([FromBody] RegisterDTO data)
+    [HttpPost("signup")]
+    public async Task<IActionResult> SelfRegisterCustomer([FromBody] RegisterDTO data)
     {
-
+        Guid userId = await authService.CreateAccount(data, "Customer");
+        return Ok(new
+        {
+            Message = "Customer registered successfully",
+            UserId = userId
+        });
     }
 }
